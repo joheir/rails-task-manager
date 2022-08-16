c62230d785b145781713c4508fd7c0243ab42f69
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-16 13:48:13 +0200

    seven thinking steps

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # # Read all tasks:
+  # get 'tasks', to: 'tasks#index'
+  # # Create a new task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # # Show a single task:
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # # Update tasks
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
